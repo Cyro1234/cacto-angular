e16443d05cf5fc9729b910a1ebdcd5454c7695f3
Cyro1234 <cyro.conde@gmail.com>
2025-01-22 16:48:45 -0300

    carrossel e card feito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
+import { CarrosselComponent } from './components/carrossel/carrossel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, CardComponent, CarrosselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cacs';
+  slides = [
+    'assets/vermelho.png',
+    'assets/verde.png',
+    'assets/azul.png'
+  ];
 }
---
+++ src/app/components/carrossel/carrossel.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-carrossel',
+  imports: [CommonModule],
+  templateUrl: './carrossel.component.html',
+  styleUrl: './carrossel.component.css'
+})
+export class CarrosselComponent {
+  @Input() slides: string[] = [];
+  indice: number = 0;
+
+  voltar() {
+    this.indice = this.indice === 0 ? this.slides.length - 1 : this.indice - 1;
+  }
+
+  avancar() {
+    this.indice = this.indice === this.slides.length - 1 ? 0 : this.indice + 1;
+  }
+
+  get transform() {
+    return `translateX(-${this.indice * 100 / this.slides.length}%)`;
+  }
+
+  get tamanhoImagem() {
+    return { width: `${100 / this.slides.length}%` };
+  }
+
+  get configBotao() {
+    return 'p-1 rounded-full shadow bg-white/90 hover:bg-white';
+  }
+}
